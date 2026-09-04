e0b917f17f647e959dca2ea86d8e8d73e74333b6
GAOSHIQING <ab1314625@qq.com>
2026-09-05 03:07:56 +0800

    性能：按窗口类型动态加载根组件拆分前端产物

--- copy-creator/src/main.tsx
+++ copy-creator/src/main.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import RadialMenu from "./components/RadialMenu";
-import ClipboardCreateDialog from "./components/ClipboardCreateDialog";
 import "./styles/index.css";
 import "./i18n";
 
@@ -10,14 +7,17 @@ const params = window.location.search;
 const isRadialWindow = params.includes("radial=1");
 const isClipboardCreateWindow = params.includes("clipboard-create=1");
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    {isRadialWindow ? (
-      <RadialMenu />
-    ) : isClipboardCreateWindow ? (
-      <ClipboardCreateDialog />
-    ) : (
-      <App />
-    )}
-  </React.StrictMode>
-);
+// 每种窗口只加载自己需要的根组件，避免弹窗窗口解析整套主应用代码。
+const loadRoot = async () => {
+  if (isRadialWindow) return (await import("./components/RadialMenu")).default;
+  if (isClipboardCreateWindow) return (await import("./components/ClipboardCreateDialog")).default;
+  return (await import("./App")).default;
+};
+
+void loadRoot().then((Root) => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+  );
+});
